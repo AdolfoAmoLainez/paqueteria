93b21377b3ee48b3b88885d7719cdbaf21aa98aa
Adolfo <adolfo.amolainez@gmail.com>
2019-02-04 15:40:10 +0100

    canvis en les urls de app.params

--- src/app/app.params.ts
+++ src/app/app.params.ts
@@ -1,8 +1,8 @@
 
 export class AppConstants {
-//public  loginURL:string = "http://localhost:3001/auth/login";
+//public  loginURL:string = "http://localhost:55023/auth/login";
 public  loginURL:string = "https://bitacola.uab.cat:55023/auth/login";
-//public dataServerURL: string = "http://localhost:3001";
+//public dataServerURL: string = "http://localhost:55023";
 public dataServerURL: string = "https://bitacola.uab.cat:55023";
 //public signUrlServer = 'http://localhost:3001/static/signarmovil/';
 public signUrlServer = 'https://bitacola.uab.cat:55023/signarmovil/';
