70be1336ac607366fadf3bd560cdba22212bc0db
Adolfo Amo <adolfo.amo@uab.cat>
2019-11-13 12:15:29 +0100

    Canvi urls entorn test bitacola

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  loginURL: 'https://chaman.uab.cat:55022/auth/login',
-  dataServerURL: 'https://chaman.uab.cat:55022',
-  signUrlServer: 'https://chaman.uab.cat:55022/signarmovil/',
+  loginURL: 'https://bitacola.uab.cat:55023/auth/login',
+  dataServerURL: 'https://bitacola.uab.cat:55023',
+  signUrlServer: 'https://bitacola.uab.cat:55023/signarmovil/',
   ADMIN: 1,
   USUARI: 2,
   rols: [
