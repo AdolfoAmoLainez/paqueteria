4e375401c2740bef9d78eb35d8a25116c9d8a76f
Adolfo Amo <adolfo.amo@uab.cat>
2020-07-21 16:14:29 +0200

    Canvis necessaris per APP-TEST al SI

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,11 @@
 export const environment = {
   production: true,
-  loginURL: 'https://testcascc.uab.cat/login',
+/*   loginURL: 'https://testcascc.uab.cat/login',
   dataServerURL: 'https://testcascc.uab.cat',
-  signUrlServer: 'https://testcascc.uab.cat/signarmovil/',
+  signUrlServer: 'https://testcascc.uab.cat/signarmovil/', */
+  loginURL: 'https://apps-test.uab.cat/paqueteria/login',
+  dataServerURL: 'https://apps-test.uab.cat/paqueteria',
+  signUrlServer: 'https://apps-test.uab.cat/paqueteria/signarmovil/',
 /*   loginURL: 'https://paqueteria.uab.cat:55023/auth/login',
   dataServerURL: 'https://paqueteria.uab.cat:55023',
   signUrlServer: 'https://paqueteria.uab.cat:55023/signarmovil/', */
